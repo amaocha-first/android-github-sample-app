2f0895ae0a84fdd02cf3e5b6a49c3b6860527c12
NishizawaShota <nishizawa@emol.jp>
2020-08-20 16:28:32 +0900

    ProjectRepositoryの追加

---
+++ app/src/main/java/com/example/droid_github_app/service/repository/ProjectRepository.kt
@@ -0,0 +1,28 @@
+package com.example.droid_github_app.service.repository
+
+import com.example.droid_github_app.service.model.Project
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val HTTPS_API_GITHUB_URL = "https://api.github.com/"
+
+class ProjectRepository {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(HTTPS_API_GITHUB_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private var githubService: GithubService = retrofit.create(GithubService::class.java)
+
+    suspend fun getProjectList(userId: String): Response<List<Project>> = githubService.getProjectList(userId)
+
+    suspend fun getProjectDetails(userID: String, projectName: String): Response<Project> = githubService.getProjectDetails(userID, projectName)
+
+    companion object Factory {
+        val instance: ProjectRepository
+            @Synchronized get() {
+                return ProjectRepository()
+            }
+    }
+}
